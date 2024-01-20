ee9e407760d43f67c0936ceb35f948b29d0e3408
gollanai <gollanai@msu.edu>
2024-01-20 16:14:29 -0500

    User routes complete (For now)

--- backend/flask_app/__init__.py
+++ backend/flask_app/__init__.py
@@ -3,13 +3,14 @@ from flask_restx import Api
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from .env import ENV_VARS
+from .routes.setup import init_api
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, doc='/api/docs')
+    api = Api(app, doc='/api/docs', title='My API', version='1.0', description='A simple API')
 
     app.config['SQLALCHEMY_DATABASE_URI'] = ENV_VARS['DATABASE_URI']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,7 +19,6 @@ def create_app():
     migrate.init_app(app, db)
 
     with app.app_context():
-        from . import routes
-        routes.init_api(api)
+        init_api(api)  # Pass the api instance to init_api
         db.create_all()
         return app
--- backend/flask_app/routes/routes.py
+++
@@ -1,7 +0,0 @@
-from flask_restx import Resource
-
-def init_api(api):
-    @api.route('/')
-    class Home(Resource):
-        def get(self):
-            return "home"
